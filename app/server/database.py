9cbd6090470d9572ef7a327912445eeecaf99075
ztrackperu <ztrack@zgroup.com.pe>
2024-04-22 06:38:34 -0500

    base12

--- app/server/database.py
+++ app/server/database.py
@@ -6,7 +6,7 @@ MONGO_DETAILS = config("MONGO_DETAILS")  # read environment variable
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 #integrado es nombre para la base de datos
-DATA_BASE = config("DATA_BASE")
+#DATA_BASE = config("DATA_BASE")
 #database = client.DATA_BASE
 #print(DATA_BASE)
 database = client.integrado

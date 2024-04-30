20d2572c48e83a1c32b1780ecf08799b08510dd4
ztrackperu <ztrack@zgroup.com.pe>
2024-04-30 12:07:42 -0500

    validarStock10

--- app/server/funciones/concepto_ot.py
+++ app/server/funciones/concepto_ot.py
@@ -442,7 +442,7 @@ async def regex_concepto_ot(des:str) :
     #reg = "conver"
     #print(reg)
     async for concepto_ot in concepto_ot_collection.find({"$and":[{"estado":1},{"descripcion":{'$regex':des,"$options" : 'i'}}]}).limit(10):
-        print(concepto_ot)
+        #print(concepto_ot)
         concepto_ots.append(concepto_ot_helper_regex(concepto_ot))
     return concepto_ots
 #,{"descripcion":regex.Regex(reg) }
